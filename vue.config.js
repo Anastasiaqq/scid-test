b515391294148c63a547e274df77e40c685d6b07
Anastasiaqq <anastasiyaberezina610@gmail.com>
2023-02-19 12:34:33 +0300

    vue.config.js redacted

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "scid-test",
+  publicPath: "/scid-test/",
   lintOnSave: false,
   transpileDependencies: true,
 });
